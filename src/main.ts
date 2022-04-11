4682e1ea154e2ff2feda0fc563d975f5a2cbeadd
zhangming9502 <zhangming9502@gmail.com>
2022-04-11 14:19:38 +0800

    feat:3. 全局路由前缀

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('nest-zero-to-one'); // 全局路由前缀
   await app.listen(3000);
 }
 bootstrap();
