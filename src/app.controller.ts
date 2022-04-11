526adee23cc8239dc1a63c7f92d8c6a966250072
zhangming9502 <zhangming9502@gmail.com>
2022-04-11 14:18:01 +0800

    feat:2. 局部路由前缀

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('lesson-1')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
